5ebfa8be5c817d9090d483dd9bbd10151ee0a21a
ElponeNote <uchang21@gmail.com>
2025-05-26 18:14:51 +0900

    Update dummy_data.dart

--- lib/utils/dummy_data.dart
+++ lib/utils/dummy_data.dart
@@ -1,19 +1,23 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/post.dart';
+import 'package:faker/faker.dart';
 
-final postsProvider = StateProvider<List<Post>>((ref) => [
-  Post(
-    id: '1',
-    author: 'User1',
-    content: '첫 번째 더미 게시글',
-    imageUrl: null,
-    createdAt: DateTime.now(),
-  ),
-  Post(
-    id: '2',
-    author: 'User2',
-    content: '두 번째 더미 게시글',
-    imageUrl: null,
-    createdAt: DateTime.now(),
-  ),
-]); 
+final faker = Faker();
+
+List<Post> generateDummyPosts(int count, {int? startId}) {
+  final now = DateTime.now();
+  return List.generate(count, (i) {
+    final id = (startId ?? 0) + i + 1;
+    return Post(
+      id: id.toString(),
+      author: faker.person.name().substring(0, 3),
+      content: faker.lorem.sentence(),
+      imageUrl: faker.randomGenerator.boolean(0.4)
+          ? null
+          : null, // 이미지 랜덤화 필요시 샘플 경로 추가
+      createdAt: now.subtract(Duration(minutes: faker.randomGenerator.integer(120))),
+    );
+  });
+}
+
+final postsProvider = StateProvider<List<Post>>((ref) => generateDummyPosts(20)); 
